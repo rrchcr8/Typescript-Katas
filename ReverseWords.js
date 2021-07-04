8ba9e32a43c3968289f97820d219e390a2204d66
Rrch <rrch.cr8@gmail.com>
2021-07-04 18:19:31 -0400

    initial commit

---
+++ ReverseWords.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.reverseWords = void 0;
+function reverseWords(str) {
+    // your code here
+    return "Go for it";
+}
+exports.reverseWords = reverseWords;
+console.log(reverseWords("perro"));
---
+++ ReverseWords.ts
@@ -0,0 +1,7 @@
+export function reverseWords(str: string): string {
+    // your code here
+    return "Go for it";
+  }
+
+ 
+  console.log(reverseWords("perro"));
